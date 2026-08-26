183807222bc6947f5f00ebbbc7b63885356b9f11
bdlmnmashrf-cmyk <bdlmnmashrf@gmail.com>
2026-08-26 19:40:53 +0300

    java

--- java/script.js
+++ java/script.js
@@ -1,47 +1,57 @@
+//
 
-var firstName="abdo"
-var age= 24
+//var firstName="abdo"
+//var age= 24
 
-console.log(`my name is + ${firstName} my age is ${age}`)
+//console.log(`my name is + ${firstName} my age is ${age}`)
 
-var n=5
-var r=7
-console.log(n+r)
-var k= "text"
-var k=20
-console.log(k/n)
-console.log(k++)
-console.log(k)
-console.log(++k)
-var l=15
-l+=10
+//var n=5
+//var r=7
+//console.log(n+r)
+//var k= "text"
+//var k=20
+//console.log(k/n)
+//console.log(k++)
+//console.log(k)
+//console.log(++k)
+//var l=15
+//l+=10
 
-console.log(l)
-console.log(k>l)
+//console.log(l)
+//console.log(k>l)
 
-var degree=prompt("inter your degree")
- if(degree>100) 
- console.log("invaled degree")
+//var degree=prompt("inter your degree")
+ //if(degree>100) 
+// console.log("invaled degree")
 
-   if (degree==null){
-       alert("please enter degree") 
+  // if (degree==null){
+    //   alert("please enter degree") 
        
-    }
-    else{
+//}
+    //else{
     
-    if (degree >= 90 && degree < 100)
-        console.log("A")
-        else if (degree>=80 && degree<90)
-        console.log("B")
-        else if (degree>=70 && degree<80)
-          console.log("c")
-       else if(degree<60)
-          console.log("f")
+   // if (degree >= 90 && degree < 100)
+    //    console.log("A")
+   //     else if (degree>=80 && degree<90)
+     //   console.log("B")
+    //    else if (degree>=70 && degree<80)
+      //    console.log("c")
+    //   else if(degree<60)
+  //        console.log("f")
 
-}
+//}
+let x= prompt("inter your trak")
+let track=["web","network","ux"]  
 
-      
-
+let flag  = track.find((item)=> item == x)
+ if (!flag)
+  track.push(x)
+  
+  
+  track.forEach((item , i , arr) => {
+ console.log(item)
+  })
+  
     
   
  
